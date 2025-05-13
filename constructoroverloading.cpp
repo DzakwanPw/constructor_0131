2f47fee0e5abeb8c407671c0d310e6186394d106
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2025-05-13 19:18:21 +0700

    commit5

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -33,5 +33,19 @@ mahasiswa :: mahasiswa(int iNim, string iNama){
 
 void mahasiswa::cetak(){
 
-    cout << endl << "Nim ="
+    cout << endl << "Nim = " <<nim <<endl;
+    cout << "Nama = " << nama << endl;
+}
+
+int main() {
+    mahasiswa mhs1(102030);
+    mahasiswa mhs2("Andi");
+    mahasiswa mhs3(010203, "Santi");
+
+    //tampilan nilai
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+
+    return 0;
 }
