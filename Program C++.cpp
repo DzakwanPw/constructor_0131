0ecb15883bfeb3e5ec830c4061583cebd3366f58
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2025-05-20 21:01:50 +0700

    tugas

--- Program C++.cpp
+++ Program C++.cpp
@@ -10,6 +10,11 @@ public:
 
 Barang::Barang(int KodeBarang, string namaBarang) {
     cout << "Constructor Dengan Parameter Terpanggil" << endl;
-    cout << "NAMA BARANG  : " << namaBarang << endl;
     cout << "KODE BARANG  : " << KodeBarang << endl;
+    cout << "NAMA BARANG  : " << namaBarang << endl;
+}
+
+int main() {
+    Barang mhs(001, "Laptop");
+    return 0;
 }
